cd7f20fb8cffa1e148adf655822070321bf0b425
VPintos2501 <manueldelmanya3@gmail.com>
2024-11-16 23:06:02 -0300

    Se corrigen errores visuales de las paginas principales, menú y header

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,18 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
-// Crear el contexto de autenticación
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Estado para almacenar el usuario logueado
+  const [user, setUser] = useState(() => {
+    // Carga los datos del usuario desde Local Storage al inicializar
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const login = (userData) => {
-    setUser(userData); // Actualiza el usuario
+    setUser(userData); // Actualiza el estado
+    localStorage.setItem("user", JSON.stringify(userData)); // Guarda en Local Storage
   };
 
   const logout = () => {
-    setUser(null); // Limpia el estado del usuario
+    setUser(null); // Limpia el estado
+    localStorage.removeItem("user"); // Elimina del Local Storage
   };
+
+  // Carga los datos del usuario desde Local Storage al iniciar la app
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
