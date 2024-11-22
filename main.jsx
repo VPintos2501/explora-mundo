655367cf7efe429821ccf7fa694839a766de5d31
VPintos2501 <manueldelmanya3@gmail.com>
2024-11-22 17:08:11 -0300

    Modificaciones para hostear

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import './index.css'; // Importa los estilos globales
+import App from './src/App.jsx'
+import './src/index.css'; // Importa los estilos globales
 
 
 createRoot(document.getElementById('root')).render(
